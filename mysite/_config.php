953bd6ab74de016b44e985b2f5901f609b33a2a4
fcuarenta <fcuarenta@reddoor.biz>
2014-08-24 20:51:21 -0700

    Arregle el config de conexion para el live site.

--- mysite/_config.php
+++ mysite/_config.php
@@ -8,45 +8,29 @@ $serverName = $_SERVER['SERVER_NAME'];
 
 //echo $serverName; 
 
-switch ($serverName) {
-    //variables for developer environment
-    case "localhost":
-            $databaseConfig = array(
-                "type" => 'MySQLDatabase',
-                "server" => 'localhost',        
-                "username" => 'naufrago',        
-                "password" => 'naufrago',
-                "database" => 'SS_buulul',        
-                "path" => '',
-            );
-            // Turn off when going live.
-            Director::set_environment_type("dev");
-        break;
-    //variables for live site environment
-    case "www.buulul.com":
-        $databaseConfig = array(
-            "type" => 'MySQLDatabase',
-            "server" => 'localhost',
-            "username" => 'insectov_buulul',
-            "password" => 'naufrago',	
-            "database" => 'insectov_buulul',
-            "path" => '',
-        );
-        break;
-    default:
-            //variables for developer environment
-            $databaseConfig = array(
-                "type" => 'MySQLDatabase',
-                "server" => 'localhost',        
-                "username" => 'buulul',        
-                "password" => 'naufrago',
-                "database" => 'SS_buulul',        
-                "path" => '',
-            );
-            // Turn off when going live.
-            Director::set_environment_type("dev");
-        break;
+if ($serverName === 'localhost'){
+   //variables for developer environment
+    $databaseConfig = array(
+        "type" => 'MySQLDatabase',
+        "server" => 'localhost',        
+        "username" => 'naufrago',        
+        "password" => 'naufrago',
+        "database" => 'SS_buulul',        
+        "path" => '',
+    );
+    // Turn off when going live.
+    Director::set_environment_type("dev"); 
+}else{
+    $databaseConfig = array(
+        "type" => 'MySQLDatabase',
+        "server" => 'localhost',
+        "username" => 'insectov_buulul',
+        "password" => 'naufrago',	
+        "database" => 'insectov_buulul',
+        "path" => '',
+    );
 }
+
 // Director::set_environment_type("dev");
 // Set the site locale
 i18n::set_locale('en_US');
